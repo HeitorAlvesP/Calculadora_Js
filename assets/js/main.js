cbfe704f76517468c33c494cdce785c211d20cf5
Heitor Alves Pinto <134733931+HeitorAlvesP@users.noreply.github.com>
2023-10-11 10:10:14 -0300

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,76 +1,50 @@
-function criaCalculadora(){
-    return{
-        display: document.querySelector('.display'), //this.display    
-        
-        inicia(){
-            this.cliqueBotoes();
-            this.precionaEnter();
-        },
+function Calculadora(){
+    this.display = document.querySelector('.display');
 
-        precionaEnter() {
-            this.display.addEventListener('Keyup', e => {
-                if(e.KeyCode === 13){
-                    this.realizaConta();
-                }
-            })
-        },
+    this.inicia = () => {
+        this.capturaClick();
+        this.prezEnter();
+    }
 
-        realizaConta() {
-            let conta = this.display.value
-            try { 
-                conta = eval(conta);
-
-                if(!conta){
-                    alert('Conta incválida');
-                    return;
-                }
-                this.display.value = String(conta);
-            } catch(e){
-                alert('Conta Inválida');
-                return;
+    this.prezEnter = () => {
+        this.display.addEventListener('keypress', e=>{
+            if(e.KeyCode === 13){
+                this.realizaConta();
             }
-        },
-        
-        clearDisplay(){
-            this.display.value = '';
-        },
+        })
+    }
 
-        apagaUm(){
-            this.display.value = this.display.value.slice(0, -1)
-        },
+    this.capturaClick = () => {
+        document.addEventListener('click', event =>{
+            const el = event.target;
+            if (el.classList.contains('btn-num')) this.addNumDisplay(el);
+            if (el.classList.contains('btn-clear')) this.clear();
+            if (el.classList.contains('btn-del')) this.del();
+            if (el.classList.contains('btn-eq')) this.realizaConta();
+        })
+    }
 
-
-
-        cliqueBotoes(){
-            document.addEventListener('click', function(e){
-                const el = e.target;
-    
-                if (el.classList.contains('btn-num')){
-                    this.btnParaDisplay(el.innerText);
-                }
-
-                if (el.classList.contains('btn-clear')){
-                    this.clearDisplay();
-                }
-
-                if (el.classList.contains('btn-del')){
-                    this.apagaUm();
-                }
-
-                if (el.classList.contains('btn-eq')){
-                    this.realizaConta();
-                }
-            }.bind(this));
-        },
-
-        btnParaDisplay(valor){
-            this.display.value += valor;
+    this.realizaConta = () => {
+        try{
+            const conta = eval(this.display.value);
+            if(!conta){
+                alert('Sua conta é invalida')
+                return
+            }
+            this.display.value = conta;
+        }catch(e){
+            alert('Sua conta é invalida');
+            return
         }
+    }
 
-
-
-    };
+    this.addNumDisplay = el => {
+        this.display.value += el.innerText;
+        this.display.focus();
+    }
+    this.clear = () => this.display.value = ''; 
+    this.del = () => this.display.value = this.display.value.slice(0, -1);
 }
 
-const calculadora = criaCalculadora();
+const calculadora = new Calculadora();
 calculadora.inicia();
